3f7549d2c1207e2f2b5bf7f17e66145b79139ea9
Diego Quintero <d86webs@gmail.com>
2026-06-19 21:32:19 -0300

    first commit

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -21,7 +21,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "any",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
   }
